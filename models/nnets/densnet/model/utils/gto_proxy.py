a1db8a9f49366789e5a3856174bf466a843d5d6a
Benni <ga48daj@mytum.de>
2023-10-02 09:41:40 +0200

    try

--- models/nnets/densnet/model/utils/gto_proxy.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import tensorflow as tf
-
-
-def exponential_tensorwise(point_R_diff_transformed):
-    # input has shape (len(Z), 3)
-    return np.exp(-np.linalg.norm(point_R_diff_transformed, axis=1))
